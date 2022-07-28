21631f9d16c6ae0ff53bc1f59c35c9dfde2568a6
Omelchenko Nataly <natom654@gmail.com>
2022-07-28 15:11:00 +0200

    my commit3 dz1less2

---
+++ src/DZ1lesson2.java
@@ -0,0 +1,7 @@
+public class DZ1lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello EugeniyaDzhu");
+        System.out.println("Hello Java Pro");
+    }
+}
