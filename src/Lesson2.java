82bbf3009a9fe641f11c007f795a797c870eaae5
Omelchenko Nataly <natom654@gmail.com>
2022-07-28 14:06:30 +0200

    my second commit

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,7 +1,7 @@
 public class Lesson2 {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("My name is Natasha");
+        System.out.println("My name is Natalia");
         System.out.println("I study in Hillel Java Pro");
     }
 }
